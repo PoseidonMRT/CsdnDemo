47e18c5bcaa7b36a13edc6da0fec60dcbb416619
tuozhaobing <1044176601@qq.com>
2017-11-14 22:27:26 +0800

    add OpenCV native Code Demo

---
+++ OpenCV02/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_study_opencv02_MainActivity_stringFromJNI(
+    JNIEnv *env,
+    jobject /* this */) {
+  std::string hello = "Hello from C++";
+
+  return env->NewStringUTF(hello.c_str());
+}
