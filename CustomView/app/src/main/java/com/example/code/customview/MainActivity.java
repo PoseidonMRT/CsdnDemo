410fd907113e54645babf1481bb04cc194d3eb03
ZhaoBingTuo <tuozhaobing@doctorwork.com>
2018-08-28 20:30:18 +0800

    添加三角向圆过渡的动画View

--- CustomView/app/src/main/java/com/example/code/customview/MainActivity.java
+++ CustomView/app/src/main/java/com/example/code/customview/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    PolarView polarView = findViewById(R.id.polar_view);
+    polarView.startAnimation(30);
+  }
 }
---
+++ CustomView/app/src/main/java/com/example/code/customview/PolarView.java
@@ -0,0 +1,98 @@
+package com.example.code.customview;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author
+ * @Date 2018/8/28
+ * @description
+ * @since 1.0.0
+ */
+public class PolarView extends View {
+
+  private int mWidth;
+  private int mHeight;
+
+  private int mTmpPointCount;
+
+  private ValueAnimator mValueAnimator;
+
+  private int mRadius = 200;
+
+  private Paint mPaint;
+
+  public PolarView(Context context) {
+    super(context);
+    init();
+  }
+
+  public PolarView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public PolarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    if (w > 0 && h > 0) {
+      mWidth = w;
+      mHeight = h;
+    }
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    canvas.drawPath(makePath(), mPaint);
+  }
+
+  public void startAnimation(int count) {
+    mValueAnimator = ValueAnimator.ofInt(3, count);
+    mValueAnimator.setDuration(5000);
+    mValueAnimator.addUpdateListener(new AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mTmpPointCount = (int) valueAnimator.getAnimatedValue();
+        postInvalidate();
+      }
+    });
+    mValueAnimator.start();
+  }
+
+  private Path makePath() {
+    Path path = new Path();
+    double angle = 2.0 * Math.PI / mTmpPointCount;
+    path.moveTo(
+        mWidth / 2 + (float) (mRadius * Math.cos(0.0)),
+        mHeight / 2 + (float) (mRadius * Math.sin(0.0)));
+    for (int i = 1; i < mTmpPointCount + 1; i++) {
+      path.lineTo(
+          mWidth / 2 + (float) (mRadius * Math.cos(angle * i)),
+          mHeight / 2 + (float) (mRadius * Math.sin(angle * i)));
+    }
+    path.close();
+    return path;
+  }
+
+  private void init() {
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mPaint.setColor(Color.BLUE);
+    mPaint.setStyle(Style.STROKE);
+    mPaint.setStrokeWidth(10);
+  }
+}
