164a19259931ae6e1775078931d639640821baaa
tuozhaobing <tuozhaobing@doctorwork.com>
2018-08-29 17:52:26 +0800

    测试

--- CustomView/app/src/main/java/com/example/code/customview/MainActivity.java
+++ CustomView/app/src/main/java/com/example/code/customview/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     PolarViewWithPoints polarView = findViewById(R.id.polar_view);
     polarView.startAnimation(8);
+
   }
 }
