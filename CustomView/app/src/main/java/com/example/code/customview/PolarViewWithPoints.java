4d643521ccdcff7e7362442d4a1550914fe8c6d0
ZhaoBingTuo <tuozhaobing@doctorwork.com>
2018-08-29 15:21:47 +0800

    添加带圆沿路径移动的动画效果

---
+++ CustomView/app/src/main/java/com/example/code/customview/PolarViewWithPoints.java
@@ -0,0 +1,118 @@
+package com.example.code.customview;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author
+ * @Date 2018/8/29
+ * @description
+ * @since 1.0.0
+ */
+public class PolarViewWithPoints extends View {
+
+  private int mWidth;
+  private int mHeight;
+
+  private int mTmpPointCount = 10;
+
+  private ValueAnimator mValueAnimator;
+
+  private int mRadius = 200;
+
+  private Paint mPaint;
+
+  private float[] mCiclePositions;
+  private PathMeasure mPathMeasure;
+  private Paint mCirclePaint;
+
+  public PolarViewWithPoints(Context context) {
+    super(context);
+    init();
+  }
+
+  public PolarViewWithPoints(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public PolarViewWithPoints(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    if (w > 0 && h > 0) {
+      mWidth = w;
+      mHeight = h;
+    }
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    canvas.drawPath(makePath(), mPaint);
+    canvas.drawCircle(mCiclePositions[0] + mWidth / 2, mCiclePositions[1] + mHeight / 2, 20,
+        mCirclePaint);
+  }
+
+  public void startAnimation(int count) {
+    mTmpPointCount = count;
+
+    mPathMeasure = new PathMeasure(makePath(), false);
+
+    mValueAnimator = ValueAnimator.ofFloat(0f, mPathMeasure.getLength());
+    mValueAnimator.setDuration(5000);
+    mValueAnimator.addUpdateListener(new AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        float value = (Float) valueAnimator.getAnimatedValue();
+        mPathMeasure.getPosTan(value, mCiclePositions, null);
+        postInvalidate();
+      }
+    });
+
+    mValueAnimator.start();
+  }
+
+  private Path makePath() {
+    Path path = new Path();
+    double angle = 2.0 * Math.PI / mTmpPointCount;
+    path.moveTo(
+        mWidth / 2 + (float) (mRadius * Math.cos(0.0)),
+        mHeight / 2 + (float) (mRadius * Math.sin(0.0)));
+    for (int i = 1; i < mTmpPointCount + 1; i++) {
+      path.lineTo(
+          mWidth / 2 + (float) (mRadius * Math.cos(angle * i)),
+          mHeight / 2 + (float) (mRadius * Math.sin(angle * i)));
+    }
+    path.close();
+    return path;
+  }
+
+  private void init() {
+    mCiclePositions = new float[2];
+
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mPaint.setColor(Color.BLUE);
+    mPaint.setStyle(Style.STROKE);
+    mPaint.setStrokeWidth(10);
+
+    mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mCirclePaint.setColor(Color.BLUE);
+    mCirclePaint.setAlpha(100);
+    mCirclePaint.setStyle(Style.FILL);
+  }
+}
